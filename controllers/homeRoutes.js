4a426e0668a1d73e7a64da8ab390e6ebf8df138a
BryceBann <109399715+BryceBann@users.noreply.github.com>
2022-11-04 15:11:13 -0500

    worked on models and controllers

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,1 @@
+const sequlize = require('../config/connection');
--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,13 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes');
+const dashBoard = require('./dashboradRoutes')
 
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
+router.use('/dashBoard', dashBoard);
+router.use((req, res) => {
+    res.status(404).end();
+});
 
 module.exports = router;
