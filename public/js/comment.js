2bba77b8c810f2716825eaf75a1362d1fa467c3b
BryceBann <109399715+BryceBann@users.noreply.github.com>
2022-11-08 17:17:13 -0600

    continued work on js files

---
+++ public/js/comment.js
@@ -0,0 +1,30 @@
+async function commentForm(event) {
+    event.preventDefault();
+
+    const commentBody = document.querySelector('input[name="commentBody"]').value.trim();
+
+    const post_id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+
+    if(commentBody) {
+        const response = await fetch('/api/comments', {
+            method: 'PUT',
+            body: JSON.stringify({
+                post_id,
+                commentBody
+            }),
+            headers: {
+                'content-Type': 'application/json'
+            }
+        });
+        if(response.ok) {
+            document.location.reload();
+        }else{
+            alert(response.statusText)
+        }
+    }
+
+}
+
+document.querySelector('.comment-form').addEventListener('click', commentForm);
