a89281caa71b133f1583b87919a58294b0c29e5c
BryceBann <109399715+BryceBann@users.noreply.github.com>
2022-11-08 18:04:48 -0600

    worked on last views files

--- seeds/post.js
+++ seeds/post.js
@@ -1,19 +1,19 @@
 const { Post } = require('../models');
 
 const postData = [{
-        title: 'Lorem Ipsum I',
-        textBody: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        title: 'why wont this page work',
+        textBody: 'The amount of time was very not cool ',
         user_id: 1
 
     },
     {
-        title: 'Lorem Ipsum II',
-        textBody: 'Amet aliquam id diam maecenas ultricies mi eget mauris pharetra.',
+        title: 'Boooooooo',
+        textBody: 'i wanna cry',
         user_id: 2
     },
     {
-        title: 'Lorem Ipsum III',
-        textBody: 'Ut etiam sit amet nisl purus in mollis.',
+        title: 'Like a whole river',
+        textBody: 'what is going on with this shit',
         user_id: 3
     }
 ];
