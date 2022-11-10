97e1848c6c296a85a50b259df25f7ca36adc14da
BryceBann <109399715+BryceBann@users.noreply.github.com>
2022-11-10 16:01:14 -0600

    still doesnt work

--- public/js/login.js
+++ public/js/login.js
@@ -11,7 +11,7 @@ const loginForm = async (event) => {
                 email,
                 password
             }),
-            header: {'Content-Type': 'application/json'}
+            headers: {'Content-Type': 'application/json'}
         });
         if(response.ok) {
             console.log('you are now logged in')
